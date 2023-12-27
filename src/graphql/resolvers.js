c7c6078376f1af0839ce60a9e6a0b83f0eee9e0f
Syed Wajahat Ali <wajahat.klk25.com>
2023-12-27 20:36:00 +0000

    pull the changes

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,7 +1,6 @@
-// import schema 
-const { typeDefs } = require('./schema'); 
-const Product = require('../models/product'); // Adjust the path accordingly
-
+// import schema
+const { typeDefs } = require("./schema");
+const Product = require("../models/product"); // Adjust the path accordingly
 
 // resolvers.js
 const resolvers = {
@@ -10,16 +9,17 @@ const resolvers = {
       if (user) {
         return `Hello, ${user.username}!`;
       }
-      return 'Not authenticated';
+      return "Not authenticated";
     },
     // New resolver for getting a list of products
     products: async () => {
+      console.log("trying fetching products");
       try {
         const products = await Product.find();
         return products;
       } catch (error) {
-        console.error('Error fetching products:', error);
-        throw new Error('Failed to fetch products');
+        console.error("Error fetching products:", error);
+        throw new Error("Failed to fetch products");
       }
     },
   },
@@ -31,8 +31,8 @@ const resolvers = {
         const savedProduct = await newProduct.save();
         return savedProduct;
       } catch (error) {
-        console.error('Error creating product:', error);
-        throw new Error('Failed to create product');
+        console.error("Error creating product:", error);
+        throw new Error("Failed to create product");
       }
     },
   },
